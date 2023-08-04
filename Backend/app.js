3bda42b746a841b527672ddd2d5106a3d0799137
ZeroPlusKikkok <vilaiphol@gmail.com>
2023-08-04 15:50:01 +0700

    From VM

--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,23 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const index = require('./routes/index');
+const api = require('./routes/api/index');
+const users = require('./routes/users');
 
 const app = express();
+
+// Connect MongoDB
+mongoose.connect('mongodb://0.0.0.0/cmmsdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('[mongoDB] Successfully connected MongoDB..'))
+  .catch((err) => console.log(err));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,9 +29,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/api', api);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
---
+++ Backend/routes/api/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Route API Express' });
+});
+
+module.exports = router;
