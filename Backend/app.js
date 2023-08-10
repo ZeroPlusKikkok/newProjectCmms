da19b613bfd8292650cb96282bb8e554708f57e1
ZeroPlusKikkok <vilaiphol@gmail.com>
2023-08-10 22:12:46 +0700

    From VM

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,6 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
