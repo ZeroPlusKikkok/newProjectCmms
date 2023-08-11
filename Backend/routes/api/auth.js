cc370fd7d56748e97975a3678f621c91c1c57c3c
ZeroPlusKikkok <vilaiphol@hotmail.com>
2023-08-11 17:25:02 +0700

    From EN01 11082023

--- Backend/routes/api/auth.js
+++ Backend/routes/api/auth.js
@@ -6,29 +6,29 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/user');
 
 // Signup to create a new user
-router.post('/signup', async (req, res, next) => {
+router.post('/signup', async (req, res) => {
   try {
     // hash the password
     req.body.password = await bcrypt.hash(req.body.password, 10);
     // create a new user
-    const user = await User.create(req,body);
+    const user = await User.create({}).then(req.body);
     // 
     res.json(user);
   } catch (error) {
-    res.status(400).json({ error });
+    res.status(400).json({ error: "Do not create.." });
   }
 });
 
 // Login to verify a user and get a token
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     // check if the user exists
     const user = await User.findOne({ username: req.body.username });
     if (user) {
-      // check if password matches
+      //check if password matches
       const result = await bcrypt.compare(req.body.password, user.password);
       if (result) {
-        //sign token and send it in response
+        // sign token and send it in response
         const token = await jwt.sign({ username: user.username }, SECRET);
         res.json({ token });
       } else {
